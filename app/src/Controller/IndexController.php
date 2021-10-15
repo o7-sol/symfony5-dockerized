b44ae7f053a796c95ef785c136916e8e26afe535
o7-sol <feknaz@gmail.com>
2021-10-15 16:11:26 +0300

    Dockerized Symfony 5 project

---
+++ app/src/Controller/IndexController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function home()
+    {
+        return new Response('te6546456st');
+    }
+}
